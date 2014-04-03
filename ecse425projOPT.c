6efa47a68445d5d7599e3592e2c94aaefd280512
Chris Despatie <christian_despatie@hotmail.com>
2014-04-03 17:16:07 -0400

    Added in-place matrix transpose function
    This function will probably be necessary/useful for maximizing
    sequential memory accesses/cache reads in our MxM implementation.

--- ecse425projOPT.c
+++ ecse425projOPT.c
@@ -2,14 +2,29 @@
 #include "ecse425proj.h"
 
 
-void matVecMult_opt(int N, const double *matA, const double *vecB, double *vecC) 
-{
-    // Code in your optimized implementation here!
-	
-}
-
-void matMult_opt(int N, const double *matA, const double *matB, double *matC) 
-{
+void matVecMult_opt(int N, const double *matA, const double *vecB, double *vecC) {
+    
     // Code in your optimized implementation here!
 }
 
+void matMult_opt(int N, const double *matA, const double *matB, double *matC) {
+   
+    // Code in your optimized implementation here!
+}
+
+// Transpose matrix matX in place [O(1) space requirement]
+static void transposeMatrix(double *matX, int N) {
+	int i, j;
+	double temp;
+
+	for (i = 0; i < N - 2; i++) {
+		for (j = i + 1; j < N - 1; j++) {
+
+			// Swap matX(i,j) and matX(j,i)
+			temp = matX[i*N + j];
+			matX[i*N + j] = matX[j*N + i];
+			matX[j*N + i] = temp;
+		}
+	}
+
+}
