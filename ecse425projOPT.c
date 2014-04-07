50c118f5bb11d77a383900dc7ead5dcd272ceedb
Chris Despatie <christian_despatie@hotmail.com>
2014-04-07 14:29:07 -0400

    Fix attempt

--- ecse425projOPT.c
+++ ecse425projOPT.c
@@ -68,11 +68,11 @@ void matMult_opt(int N, const double *matA, const double *matB, double *matC) {
     				for (y = j; y < MIN(j + B, N); y++) {
     					for (z = m; z < MIN(m + B, N); z++) {
 
-    						temp += matA[x*N + z] * matY[y*N + z];
+    						temp += matA[x*MIN(m + B, N) + z] * matY[y*MIN(m + B, N) + z];
 
     					}
 
-    					matC[y + x*N] = temp;
+    					matC[y + x*MIN(j + B, N)] = temp;
     					temp = 0.0;
     				}
     			}
